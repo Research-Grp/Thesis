8630194f7dbffad27485df5506d5d14c6d3fc5fc
FactBoy <50394333+FactBoy@users.noreply.github.com>
2021-11-13 21:02:12 +0800

    added files

--- PrescriptionApp.py
+++ PrescriptionApp.py
@@ -7,8 +7,10 @@ from kivymd.uix.screen import MDScreen
 from kivy.properties import ObjectProperty
 from kivy.uix.behaviors import DragBehavior
 from kivy.uix.label import Label
+from kivy.core.window import Window
 
-
+class LoadingScreen(MDScreen):
+    pass
 class HomeScreen(MDScreen):
     pass
 class AboutScreen(MDScreen):
@@ -19,14 +21,20 @@ class DeveloperScreen(MDScreen):
 class WindowManager(ScreenManager):
     screen_manager = ObjectProperty(None)
 
+
 class DragLabel(DragBehavior, Label):
     pass
 
 
 class MyApp(MDApp):
     def build(self):
+        Window.bind(on_dropfile=self._on_file_drop)
         self.root_widget = Builder.load_file("style.kv")
         return self.root_widget
+
+    def _on_file_drop(self, window, file_path):
+        print(file_path)
+        return
 
 if __name__ == "__main__":
     MyApp().run()
